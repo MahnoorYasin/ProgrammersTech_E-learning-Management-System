e88ef940c4d226b56ad096d82bc30800eb49079b
Mahnoor Yasin <124428375+MahnoorYasin@users.noreply.github.com>
2024-07-12 13:12:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+    const faqItems = document.querySelectorAll('.faq-item');
+
+    faqItems.forEach(item => {
+        const question = item.querySelector('.faq-question');
+
+        question.addEventListener('click', () => {
+            item.classList.toggle('active');
+        });
+    });
+});
